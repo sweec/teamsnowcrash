7f87d0edf1ad9d7bfe6718ffa3564f13d0087ba6
dearnest <dearnest@localhost>
2010-09-15 12:04:48 -0400

    First commit, includes prototype critter

---
+++ src/org/snowcrash/prototype/ProtoCritter.java
@@ -0,0 +1,49 @@
+package org.snowcrash.prototype;
+
+public class ProtoCritter {
+
+	private String name;
+	private String type;
+	private int vision;
+	private int attack;
+	private int movement;
+	private int health;
+
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	public int getVision() {
+		return vision;
+	}
+	public void setVision(int vision) {
+		this.vision = vision;
+	}
+	public int getAttack() {
+		return attack;
+	}
+	public void setAttack(int attack) {
+		this.attack = attack;
+	}
+	public int getMovement() {
+		return movement;
+	}
+	public void setMovement(int movement) {
+		this.movement = movement;
+	}
+	public int getHealth() {
+		return health;
+	}
+	public void setHealth(int health) {
+		this.health = health;
+	}
+
+}
