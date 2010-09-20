3660cb6c92670912afed673844fd1efc31712dc1
dong <dong@localhost>
2010-09-20 13:28:31 -0400

    initial implementation of file utilities

--- src/org/snowcrash/prototype/FileUtilities.java
+++ src/org/snowcrash/prototype/FileUtilities.java
@@ -2,6 +2,8 @@ package org.snowcrash.prototype;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 
@@ -12,10 +14,19 @@ public class FileUtilities {
 		try { 
 			BufferedReader in = new BufferedReader(new FileReader(filename)); 
 			String str; 
-
+			ProtoCritter critter;
+			
 			while ((str = in.readLine()) != null) { 
 				String[] tokens = str.split(",");
-				//TODO: Implement tokenization
+				if (tokens.length < 6) continue;
+				critter = new ProtoCritter();
+				critter.setName(tokens[0]);
+				critter.setType(tokens[1]);
+				critter.setVision(Integer.parseInt(tokens[2]));
+				critter.setAttack(Integer.parseInt(tokens[3]));
+				critter.setMovement(Integer.parseInt(tokens[4]));
+				critter.setHealth(Integer.parseInt(tokens[5]));
+				contents.add(critter);
 			} 
 			in.close(); 
 		} catch (IOException e) { 
@@ -24,8 +35,25 @@ public class FileUtilities {
 		return contents;
 	}
 
+	public static void saveCritterToFile(ArrayList<ProtoCritter> contents, String filename) {
+		try { 
+			BufferedWriter out = new BufferedWriter(new FileWriter(filename)); 
+			ProtoCritter critter;
+			int i;
+			for (i = 0;i < contents.size();i++) {
+				critter = contents.get(i);
+				out.write(critter.getName()+","+critter.getType()+","+critter.getVision()+",");
+				out.write(critter.getAttack()+","+critter.getMovement()+","+critter.getHealth());
+				out.newLine();
+			} 
+			out.close(); 
+		} catch (IOException e) { 
+			e.printStackTrace();
+		} 		
+	}
 	public static void main(String[] args) {
 		ArrayList<ProtoCritter> contents = getCritterFileContents("critters.csv");
-		contents.toString();
+		System.out.println(contents.toString());
+		//saveCritterToFile(contents, "testOut.csv");
 	}
 }
