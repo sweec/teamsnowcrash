00d1bc9532be697fb78ec19aa833c99fda8950b9
dong <dong@localhost>
2010-09-27 14:42:31 -0400

    Added initial json version of FileUtilities.

--- src/org/snowcrash/prototype/FileUtilities.java
+++ src/org/snowcrash/prototype/FileUtilities.java
@@ -6,28 +6,45 @@ import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
+import com.google.gson.JsonStreamParser;
+import com.google.gson.JsonElement;
+import com.google.gson.Gson;
 
 public class FileUtilities {
+	static private boolean useCSV = false;
+	static private boolean useJson = true;
 
 	public static ArrayList<ProtoCritter> getCritterFileContents(String filename) {
 		ArrayList<ProtoCritter> contents = new ArrayList<ProtoCritter>();
 		try { 
 			BufferedReader in = new BufferedReader(new FileReader(filename)); 
-			String str; 
 			ProtoCritter critter;
 			
-			while ((str = in.readLine()) != null) { 
-				String[] tokens = str.split(",");
-				if (tokens.length < 6) continue;
-				critter = new ProtoCritter();
-				critter.setName(tokens[0]);
-				critter.setType(tokens[1]);
-				critter.setVision(Integer.parseInt(tokens[2]));
-				critter.setAttack(Integer.parseInt(tokens[3]));
-				critter.setMovement(Integer.parseInt(tokens[4]));
-				critter.setHealth(Integer.parseInt(tokens[5]));
-				contents.add(critter);
-			} 
+			if (useCSV) {
+				String str; 
+				while ((str = in.readLine()) != null) { 
+					String[] tokens = str.split(",");
+					if (tokens.length < 6) continue;
+					critter = new ProtoCritter();
+					critter.setName(tokens[0]);
+					critter.setType(tokens[1]);
+					critter.setVision(Integer.parseInt(tokens[2]));
+					critter.setAttack(Integer.parseInt(tokens[3]));
+					critter.setMovement(Integer.parseInt(tokens[4]));
+					critter.setHealth(Integer.parseInt(tokens[5]));
+					contents.add(critter);
+				}
+			}
+			if (useJson) {
+				JsonStreamParser parser = new JsonStreamParser(in);
+				JsonElement element;
+				Gson gson = new Gson();
+				while (parser.hasNext()) {
+					element = parser.next();
+					critter = gson.fromJson(element, ProtoCritter.class);
+					contents.add(critter);
+				}
+			}
 			in.close(); 
 		} catch (IOException e) { 
 			e.printStackTrace();
@@ -39,21 +56,34 @@ public class FileUtilities {
 		try { 
 			BufferedWriter out = new BufferedWriter(new FileWriter(filename)); 
 			ProtoCritter critter;
+			Gson gson = new Gson();
+			
 			int i;
 			for (i = 0;i < contents.size();i++) {
 				critter = contents.get(i);
-				out.write(critter.getName()+","+critter.getType()+","+critter.getVision()+",");
-				out.write(critter.getAttack()+","+critter.getMovement()+","+critter.getHealth());
-				out.newLine();
-			} 
+				if (useCSV) {
+					out.write(critter.getName()+","+critter.getType()+","+critter.getVision()+",");
+					out.write(critter.getAttack()+","+critter.getMovement()+","+critter.getHealth());
+					out.newLine();
+				}
+				if (useJson) {
+					gson.toJson(critter, out);
+				}
+			}
 			out.close(); 
 		} catch (IOException e) { 
 			e.printStackTrace();
 		} 		
 	}
 	public static void main(String[] args) {
+		useCSV = true;
+		useJson = false;
 		ArrayList<ProtoCritter> contents = getCritterFileContents("critters.csv");
-		System.out.println(contents.toString());
-		//saveCritterToFile(contents, "testOut.csv");
+		System.out.println(contents);
+		useCSV = false;
+		useJson = true;
+		saveCritterToFile(contents, "critters.JSon");
+		contents = getCritterFileContents("critters.JSon");
+		System.out.println(contents);
 	}
 }
