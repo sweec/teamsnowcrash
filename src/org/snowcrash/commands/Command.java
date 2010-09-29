17a6b3f83013767c44d68e430d3e5a0e848e355f
Mike <Mike@localhost>
2010-09-28 22:12:59 -0400

    Fixed formatting.  >.<

--- src/org/snowcrash/commands/Command.java
+++ src/org/snowcrash/commands/Command.java
@@ -1,53 +1,37 @@
 package org.snowcrash.commands;
 
-
 /**
  * 
- * The Command enum provides a pre-defined number of ways for the GUI (or other 
- * parts of the application) to interact with the services.  Basically, each command 
- * is a portal to a use case implementation.
+ * The Command enum provides a pre-defined number of ways for the GUI (or other
+ * parts of the application) to interact with the services. Basically, each
+ * command is a portal to a use case implementation.
  * 
  * @author Mike
- *
+ * 
  */
-public enum Command
-{
+public enum Command {
 	// -- Configuration commands.
-	IMPORT_CRITTER,
-	EXPORT_CRITTER,
-	CREATE_CRITTER,
-	MODIFY_CRITTER,
-	DELETE_CRITTER,
-	
+	IMPORT_CRITTER, EXPORT_CRITTER, CREATE_CRITTER, MODIFY_CRITTER, DELETE_CRITTER,
+
 	// -- Simulation commands.
-	PLAY_SIM,
-	PAUSE_SIM,
-	STOP_SIM,
-	LOAD_SIM,
-	SAVE_SIM,
-	
+	PLAY_SIM, PAUSE_SIM, STOP_SIM, LOAD_SIM, SAVE_SIM,
+
 	// -- Results commands.
-	VIEW_RESULTS,
-	LOAD_RESULTS,
-	SAVE_RESULTS,
-	
+	VIEW_RESULTS, LOAD_RESULTS, SAVE_RESULTS,
+
 	// -- Other commands.
-	RESET,
-	EXIT,
-	TEST;
-	
-	private Command()
-	{
+	RESET, EXIT, TEST;
+
+	private Command() {
 		// -- Enum.
 	}
-	
+
 	/**
 	 * 
 	 * This method launches the Command so it can be handled appropriately.
 	 * 
 	 */
-	public void launch()
-	{
-		CommandMediator.handleCommand( this );
+	public void launch() {
+		CommandMediator.handleCommand(this);
 	}
 }
