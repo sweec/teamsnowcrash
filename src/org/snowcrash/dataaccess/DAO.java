bddc457404ad5de250f01ffd2a84e7639b7e178c
Mike <Mike@localhost>
2010-11-06 13:41:40 -0400

    Added comments.

--- src/org/snowcrash/dataaccess/DAO.java
+++ src/org/snowcrash/dataaccess/DAO.java
@@ -1,13 +1,63 @@
 package org.snowcrash.dataaccess;
 
-public interface DAO {
-	public void create(Object o);
 
-	public Object read(int id);
+/**
+ * 
+ * This interface defines the methods that all DAOs must implement.
+ * 
+ * @author Mike
+ *
+ */
+public interface DAO
+{
+	/**
+	 * 
+	 * Creates an object in the database.
+	 * 
+	 * @param o the object to create
+	 * @throws DAOException
+	 * 
+	 */
+	public void create(Object o) throws DAOException;
 
-	public void update(Object o);
+	/**
+	 * 
+	 * Reads an object from the database.
+	 * 
+	 * @param id the ID of the object to read
+	 * @return the object
+	 * @throws DAOException
+	 * 
+	 */
+	public Object read(int id) throws DAOException;
 
-	public void delete(Object o);
+	/**
+	 * 
+	 * Updates an object in the database.
+	 * 
+	 * @param o the object to update
+	 * @throws DAOException
+	 * 
+	 */
+	public void update(Object o) throws DAOException;
 
-	public void delete(int id);
+	/**
+	 * 
+	 * Deletes an object from the database.
+	 * 
+	 * @param o the object to delete
+	 * @throws DAOException
+	 * 
+	 */
+	public void delete(Object o) throws DAOException;
+
+	/**
+	 * 
+	 * Deletes an object from the database.
+	 * 
+	 * @param id the ID of the object to delete
+	 * @throws DAOException
+	 * 
+	 */
+	public void delete(int id) throws DAOException;
 }
