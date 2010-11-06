29422108b46c7e687f08ef46a3c1d80aa6bdda22
Mike <Mike@localhost>
2010-11-06 12:41:07 -0400

    Added comments.

--- src/org/snowcrash/commands/Command.java
+++ src/org/snowcrash/commands/Command.java
@@ -1,7 +1,20 @@
 package org.snowcrash.commands;
 
 
+/**
+ * 
+ * This interface defines the available public methods that command subclasses must 
+ * implement.
+ * 
+ * @author Mike
+ *
+ */
 public interface Command
 {
+	/**
+	 * 
+	 * Executes this command.
+	 * 
+	 */
 	public void execute();
 }
