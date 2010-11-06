d20e819c1b59645c4d7e359e1552f67c7436694d
Mike <Mike@localhost>
2010-11-06 12:39:36 -0400

    Updates to work for critter templates.  Was previously critters, as a placeholder.

--- src/org/snowcrash/filemanagement/IFileManager.java
+++ src/org/snowcrash/filemanagement/IFileManager.java
@@ -1,10 +1,38 @@
 package org.snowcrash.filemanagement;
 
-import org.snowcrash.critter.Critter;
+import org.snowcrash.critter.CritterTemplate;
 
-public interface IFileManager {
-	public boolean saveCritters(Critter[] critter, String filepath,
-			String filename);
 
-	public Critter loadCritters(String filepath, String filename);
+/**
+ * 
+ * This interface defines the methods of file manager implementations.
+ * 
+ * @author Mike
+ *
+ */
+public interface IFileManager
+{
+	/**
+	 * 
+	 * Saves the given critter templates to a file.
+	 * 
+	 * @param critterTemplates the critter templates to save
+	 * @param filepath the file path of the file
+	 * @param filename the file name of the file
+	 * @return true, if it saved successfully; false, otherwise
+	 * 
+	 */
+	public boolean saveCritterTemplates(CritterTemplate[] critterTemplates, 
+			String filepath, String filename);
+
+	/**
+	 * 
+	 * Loads critter templates from a file.
+	 * 
+	 * @param filepath the file path of the file
+	 * @param filename the file name of the file
+	 * @return an array of critter templates from the file
+	 * 
+	 */
+	public CritterTemplate[] loadCritterTemplates(String filepath, String filename);
 }
