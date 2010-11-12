4161204e616e7bb2cfb20475cf7304c0f821cbc1
Mike <Mike@localhost>
2010-11-12 14:38:19 -0500

    Constants file.

---
+++ src/org/snowcrash/dataaccess/DAOExceptionMessages.java
@@ -0,0 +1,9 @@
+package org.snowcrash.dataaccess;
+
+
+interface DAOExceptionMessages
+{
+	static final String ALREADY_EXISTS_MESSAGE = "Data already exists in the database.";
+	static final String UNSUPPORTED_TYPE_MESSAGE = "UnsupportedType %s.";
+	static final String DOES_NOT_EXIST_MESSAGE = "Object does not exist in the database.";	
+}
