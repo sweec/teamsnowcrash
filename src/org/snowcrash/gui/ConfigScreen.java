6fe6fba7e8b60556cb2280317ff3355d75f58ace
Mike <Mike@localhost>
2010-11-22 21:12:35 -0500

    Added import to compile.

--- src/org/snowcrash/gui/ConfigScreen.java
+++ src/org/snowcrash/gui/ConfigScreen.java
@@ -6,6 +6,7 @@ import java.awt.Dimension;
 import javax.swing.Box;
 import javax.swing.BoxLayout;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 import javax.swing.JTabbedPane;
 
 public class ConfigScreen extends BaseGUI
